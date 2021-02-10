5cd839fbca8392b3b8b2cc6b6b5f712b58535f6b
LoveSy <shana@zju.edu.cn>
2021-02-10 17:43:48 +0800

    Use instance

--- service/src/main/java/io/github/xposed/xposedservice/XposedService.java
+++ service/src/main/java/io/github/xposed/xposedservice/XposedService.java
@@ -1,15 +1,30 @@
 package io.github.xposed.xposedservice;
 
 import android.os.IBinder;
+import android.os.RemoteException;
 
-public class XposedService {
+public class XposedService implements IXposedService {
     private static IBinder service_binder = null;
     private static IXposedService service = null;
+    private static XposedService instance = null;
 
-    public static IXposedService getService() {
+    public static XposedService getService() {
         if (service_binder != null && service != null) {
             service = IXposedService.Stub.asInterface(service_binder);
         }
-        return service;
+        if (instance == null) {
+            instance = new XposedService();
+        }
+        return instance;
+    }
+
+    @Override
+    public int getVersion() throws RemoteException {
+        return service.getVersion();
+    }
+
+    @Override
+    public IBinder asBinder() {
+        return service_binder;
     }
 }
